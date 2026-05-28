1bb992d410a363be7844c328a37dd2560a05217a
Intranox <118127792+Intranox@users.noreply.github.com>
2026-05-28 19:40:48 +0200

    Update snapshot.dart

--- pws_watcher/lib/pages/snapshot/snapshot.dart
+++ pws_watcher/lib/pages/snapshot/snapshot.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:overlay_support/overlay_support.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_view/photo_view.dart';
-import 'package:gal/gal.dart';
-import 'package:http/http.dart' as http;
 
 class SnapshotPage extends StatefulWidget {
-  SnapshotPage(
+  const SnapshotPage(
     this.urlImage,
     this.title, {
+    super.key,
     this.description,
     this.download = false,
     this.downloadName,
@@ -26,11 +29,12 @@ class SnapshotPage extends StatefulWidget {
   final String? downloadName;
 
   @override
-  _SnapshotPageState createState() => _SnapshotPageState();
+  State<SnapshotPage> createState() => _SnapshotPageState();
 }
 
 class _SnapshotPageState extends State<SnapshotPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   int _requestCounter = 0;
 
   @override
@@ -42,36 +46,44 @@ class _SnapshotPageState extends State<SnapshotPage> {
   @override
   Widget build(BuildContext context) {
     return Dismissible(
-      key: Key('dismissible'),
+      key: const Key('dismissible'),
       direction: DismissDirection.vertical,
-      onDismissed: (direction) {
+      onDismissed: (_) {
         Navigator.pop(context);
       },
       background: Container(
-        color: widget.backgroundColor,
+        color: widget.backgroundColor ?? Colors.black,
       ),
-      movementDuration: Duration(milliseconds: 100),
-      resizeDuration: Duration(milliseconds: 100),
+      movementDuration: const Duration(milliseconds: 100),
+      resizeDuration: const Duration(milliseconds: 100),
       child: Scaffold(
         key: _scaffoldKey,
-        backgroundColor: widget.backgroundColor,
+        backgroundColor: widget.backgroundColor ?? Colors.black,
         appBar: widget.title != null
             ? AppBar(
-                iconTheme: IconThemeData(color: Colors.white),
+                iconTheme: const IconThemeData(color: Colors.white),
                 backgroundColor: Colors.black,
                 title: Text(
                   widget.title!,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.white),
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(color: Colors.white),
                 ),
                 actions: <Widget>[
                   IconButton(
-                      icon: Icon(Icons.refresh),
-                      onPressed: () => this.setState(() {
-                            _requestCounter++;
-                          })),
-                  widget.download
-                      ? IconButton(icon: Icon(Icons.file_download), onPressed: () => _downloadImageFromUrl())
-                      : Container(),
+                    icon: const Icon(Icons.refresh),
+                    onPressed: () {
+                      setState(() {
+                        _requestCounter++;
+                      });
+                    },
+                  ),
+                  if (widget.download)
+                    IconButton(
+                      icon: const Icon(Icons.file_download),
+                      onPressed: _downloadImageFromUrl,
+                    ),
                 ],
               )
             : null,
@@ -79,65 +91,139 @@ class _SnapshotPageState extends State<SnapshotPage> {
           fit: StackFit.expand,
           children: <Widget>[
             _getPhotoView(),
-            widget.description != null
-                ? Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Container(
-                      color: Colors.black45,
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        widget.description!,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.white),
-                      ),
-                    ),
-                  )
-                : new Container(),
+            if (widget.description != null)
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  color: Colors.black45,
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    widget.description!,
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
           ],
         ),
       ),
     );
   }
 
-  Future<Null> _downloadImageFromUrl() async {
-    PermissionStatus permission = await Permission.storage.status;
+  Future<void> _downloadImageFromUrl() async {
+    try {
+      PermissionStatus permission;
 
-    if (!permission.isGranted) {
-      PermissionStatus response = await Permission.storage.request();
+      if (Platform.isAndroid) {
+        permission = await Permission.storage.request();
+      } else {
+        permission = await Permission.photos.request();
+      }
 
-      if (!response.isGranted) {
+      if (!permission.isGranted) {
         showSimpleNotification(
-          Text("Please grant the permissions to download the image 🙏🏻"),
-          background: Colors.grey[800],
+          const Text("Permesso negato"),
+          background: Colors.red,
           foreground: Colors.white,
-          autoDismiss: true,
-          duration: Duration(seconds: 2),
-          position: NotificationPosition.bottom,
-          slideDismissDirection: DismissDirection.down,
+          duration: const Duration(seconds: 2),
         );
         return;
       }
-    }
-    try {
-      final response = await http.get(Uri.parse(widget.urlImage!));
-      await Gal.putImageBytes(response.bodyBytes, name: widget.downloadName! + ".png");
-    } catch (error) {
-      print(error);
+
+      final response = await http.get(
+        Uri.parse(widget.urlImage!),
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception("Download fallito");
+      }
+
+      Directory directory;
+
+      if (Platform.isAndroid) {
+        directory = Directory('/storage/emulated/0/Download');
+      } else {
+        directory = await getApplicationDocumentsDirectory();
+      }
+
+      if (!await directory.exists()) {
+        await directory.create(recursive: true);
+      }
+
+      final fileName =
+          "${widget.downloadName ?? DateTime.now().millisecondsSinceEpoch}.png";
+
+      final file = File('${directory.path}/$fileName');
+
+      await file.writeAsBytes(response.bodyBytes);
+
+      showSimpleNotification(
+        Text("Immagine salvata:\n$fileName"),
+        background: Colors.green,
+        foreground: Colors.white,
+        duration: const Duration(seconds: 3),
+        position: NotificationPosition.bottom,
+      );
+    } catch (e) {
+      debugPrint(e.toString());
+
+      showSimpleNotification(
+        const Text("Errore durante il download"),
+        background: Colors.red,
+        foreground: Colors.white,
+        duration: const Duration(seconds: 3),
+        position: NotificationPosition.bottom,
+      );
     }
   }
 
   Widget _getPhotoView() {
-    String url = widget.urlImage! +
-        (widget.urlImage!.contains("?") ? ("&n=" + _requestCounter.toString()) : ("?n=" + _requestCounter.toString()));
+    final String url = widget.urlImage! +
+        (widget.urlImage!.contains("?")
+            ? "&n=$_requestCounter"
+            : "?n=$_requestCounter");
+
     return PhotoView.customChild(
       childSize: const Size(100, 100),
-      backgroundDecoration: BoxDecoration(
+      backgroundDecoration: const BoxDecoration(
         color: Colors.transparent,
       ),
       child: Container(
         padding: widget.padding,
         color: Colors.transparent,
-        child: Image.network(url),
+        child: Image.network(
+          url,
+          fit: BoxFit.contain,
+          loadingBuilder: (
+            BuildContext context,
+            Widget child,
+            ImageChunkEvent? loadingProgress,
+          ) {
+            if (loadingProgress == null) {
+              return child;
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+          errorBuilder: (
+            BuildContext context,
+            Object error,
+            StackTrace? stackTrace,
+          ) {
+            return const Center(
+              child: Icon(
+                Icons.broken_image,
+                color: Colors.white,
+                size: 60,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
